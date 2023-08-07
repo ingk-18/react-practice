82c89349d5867544ae331505dfd013b16bd9d557
ingk-18 <64448438+ingk-18@users.noreply.github.com>
2023-08-08 08:04:12 +0900

    クリーンアップ処理

--- src/App_hoge.jsx
+++ src/App_hoge.jsx
@@ -1,16 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-const Unmount = () => {
-  const [mount, setUnmount] = useState(true);
-
-  return (
-    <>
-      {mount && <Timer />}
-      <button onClick={() => setUnmount((prev) => !prev)}>unMount</button>
-    </>
-  );
-};
-
 const Timer = () => {
   const [count, setCount] = useState(0);
 
@@ -19,9 +8,8 @@ const Timer = () => {
       setCount((prevCount) => prevCount + 1);
     }, 1000);
 
-    // クリーンアップ処理
     return () => {
-      console.log("クリーンアップを実行");
+      console.log("クリーンアップ処理実行");
       clearInterval(intervalId);
     };
   }, [count]);
@@ -29,4 +17,4 @@ const Timer = () => {
   return <div>Count: {count}</div>;
 };
 
-export default Unmount;
+export default Timer;
